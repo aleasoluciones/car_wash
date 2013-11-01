f2e82fe4cd2cc683079ad1a7d833ee112b78fc85
eferro <eduardo.ferro.aldama@gmail.com>
2013-11-01 09:38:49 +0100

    Refactor

--- specs/car_wash_spec.py
+++ specs/car_wash_spec.py
@@ -10,24 +10,30 @@ def create_car_wash_service(sms_sender):
     return service.CarWashService(sms_sender)
 
 
+car1 = car.Car(plate='123-XXX')
+car2 = car.Car(plate='666-XXX')
+car3 = car.Car(plate='777-XXX')
+customer1 = customer.Customer(name='customer1', mobile_phone='123')
+customer2 = customer.Customer(name='customer2', mobile_phone='666')
+customer3 = customer.Customer(name='customer1', mobile_phone='777')
+
 with describe('Car wash service') as _:
+
+    @before.each
+    def set_up():
+        _.sms_sender = Spy()
+        _.car_wash_service = create_car_wash_service(_.sms_sender)
 
     with context('customer notification'):
 
         with describe('when service completed'):
 
             def it_notifies_the_customer():
-                sms_sender = Spy()
-                a_car = car.Car(plate='1234-XXX')
-                a_customer = customer.Customer(name='Irrelevant name', mobile_phone='555555555')
-                car_wash_service = create_car_wash_service(sms_sender)
+                service_id = _.car_wash_service.require_car_wash(car1, customer1)
+                _.car_wash_service.wash_completed(service_id)
 
-                service_id = car_wash_service.require_car_wash(a_car, a_customer)
-                car_wash_service.wash_completed(service_id)
-
-                assert_that(sms_sender.send,
-                    called().with_args(mobile_phone='555555555',
-                        text=contains_string('1234-XXX')))
+                assert_that(_.sms_sender.send,
+                    called().with_args(mobile_phone='123', text=contains_string('123-XXX')))
 
     with context('reporting'):
 
